d36cb0e521ec4c5359dc5c3ac55ac4f6fa485dea
Jhoselin <jhoselin.cas03@gmail.com>
2025-09-16 10:49:10 -0400

    CV adaptado en angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from './header/header';
+import { Summary } from './summary/summary';
+import { Education } from './education/education';
+import { Skills } from './skills/skills';
+import { Experience } from './experience/experience';
+import { Footer } from './footer/footer';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+            Header,
+            Summary,
+            Education,
+            Skills,
+            Experience,
+            Footer
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('CV-Jhoselin');
 }
+
